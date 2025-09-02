78355ee97155e58243db84b6020786b27d9cc433
evanbrookes1 <exb579@student.bham.ac.uk>
2025-09-02 16:36:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1279,5 +1279,6 @@ def get_trade_signals():
 
 
 # --- Start the Server ---
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
